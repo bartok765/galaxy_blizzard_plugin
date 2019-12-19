49c16008d24e699942bc1727e39f0cad9d2b42ca
Bartok <bartok765@protonmail.com>
2019-12-19 12:18:39 +0100

    fix build task.py

--- tasks.py
+++ tasks.py
@@ -2,7 +2,8 @@ import os
 import sys
 import json
 import tempfile
-from shutil import rmtree, copy, copytree
+from shutil import rmtree
+from distutils.dir_util import copy_tree
 
 from invoke import task
 from galaxy.tools import zip_folder_to_file
@@ -13,24 +14,27 @@ from src.version import __version__
 @task
 def build(c, target="build"):
     if os.path.exists(target):
+        print('--> Removing {} directory'.format(target))
         rmtree(target)
 
-    # firstly flat dependencies pip-compile as pip requires --no-deps for foreign platform dependencies
+    # Firstly dependencies needs to be "flatten" with pip-compile as pip requires --no-deps if --platform is used
+    print('--> Flattening dependencies to temporary requirements file')
     with tempfile.NamedTemporaryFile(mode="w", delete=False) as tmp:
         c.run(f'pip-compile requirements/app.txt --output-file=-', out_stream=tmp)
 
-    # then install all stuff with pip to target folder
+    # Then install all stuff with pip to target folder
+    print('--> Installing with pip for specific version')
     args = [
         'pip', 'install',
         '-r', tmp.name,
         '--python-version', '37',
         '--platform', sys.platform,
         '--target', target,
-        '--no-compile',
+        '--no-compile'
         '--no-deps'
     ]
     c.run(" ".join(args), echo=True)
     os.unlink(tmp.name)
 
-    # copy source files
-    copytree("src", target)
+    print('--> Copying source files')
+    copy_tree("src", target)
