7dbbafc42fe1e9df869071bc50eaf31705016c80
bartok765 <bartok765@protonmail.com>
2021-05-28 15:29:09 +0200

    Improved test with unhandled license.

--- tests/test_owned_games.py
+++ tests/test_owned_games.py
@@ -43,9 +43,15 @@ async def test_owned_games(pg, backend_mock, backend_owned_games, backend_no_cla
     assert sorted(result, key=lambda x: x.game_id) == sorted(result_owned_games, key=lambda x: x.game_id)
 
 
+@pytest.mark.parametrize("game_account_status", [
+    "NEW_STATUS",
+    True,
+    None,
+    0
+])
 @pytest.mark.asyncio
-async def test_owned_games_with_unknown_licenses(pg, backend_mock, backend_owned_games, result_owned_games):
-    backend_owned_games["gameAccounts"][1]['gameAccountStatus'] = "NEW_STATUS"
+async def test_owned_games_with_unknown_licenses(pg, backend_mock, backend_owned_games, result_owned_games, game_account_status):
+    backend_owned_games["gameAccounts"][1]['gameAccountStatus'] = game_account_status
     result_owned_games[1] = Game("odin", "Call of Duty: Modern Warfare", None, LicenseInfo(LicenseType.Unknown))
     backend_mock.get_owned_games.return_value = backend_owned_games
     backend_mock.get_owned_classic_games.return_value = {"classicGames": []}
