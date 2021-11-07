4386ee2902cfaf4f7871613d419ad6f2ddad6e77
BradB111 <brad.barlow111@gmail.com>
2021-11-07 12:11:04 +0000

    Update definitions.py

--- src/definitions.py
+++ src/definitions.py
@@ -83,7 +83,7 @@ class _Blizzard(object, metaclass=Singleton):
         1381257807: RegionalGameInfo('rtro', False),
         1464615513: RegionalGameInfo('wlby', False),
         5198665: RegionalGameInfo('osi', False),
-	    1179603525: RegionalGameInfo('fore', False)
+	1179603525: RegionalGameInfo('fore', False)
     }
     TITLE_ID_MAP_CN = {
         **TITLE_ID_MAP,
@@ -107,7 +107,7 @@ class _Blizzard(object, metaclass=Singleton):
         BlizzardGame('rtro', 'Blizzard Arcade Collection', 'RTRO'),
         BlizzardGame('wlby', 'Crash Bandicoot 4: It\'s About Time', 'WLBY'),
         BlizzardGame('osi', 'Diablo® II: Resurrected', 'OSI'),
-	    BlizzardGame('fore', 'Call of Duty: Vanguard', 'FORE')
+	BlizzardGame('fore', 'Call of Duty: Vanguard', 'FORE')
     ]
     CLASSIC_GAMES = [
         ClassicGame('d2', 'Diablo® II', 'Diablo II', 'Diablo II', 'DisplayIcon', "Game.exe", "com.blizzard.diabloii"),
